95e48fd2ede44b7d668980944fb3710b9563b9cb
Volker Lieber <42102008+VolkerLieber@users.noreply.github.com>
2025-01-31 18:57:57 +0100

    [Improve] Added option to search for currency by unique symbol
    Currencies with the same symbols can not be found by this!
    This function is needed because of a really bad API we have to get data from, which switches between currency symbol and currency code in its responses.

--- tests/currency_test.go
+++ tests/currency_test.go
@@ -1,9 +1,10 @@
 package tests
 
 import (
+	"testing"
+
 	"github.com/gouef/currency"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestFindCurrency(t *testing.T) {
@@ -67,6 +68,48 @@ func TestValidateCurrency(t *testing.T) {
 		t.Run(test.code, func(t *testing.T) {
 			result := currency.ValidateCurrency(test.code)
 			assert.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestFindUniqueSymbolCurrency(t *testing.T) {
+	tests := []struct {
+		symbol   string
+		expected *currency.Currency
+		hasError bool
+	}{
+		{
+			symbol: "€",
+			expected: &currency.Currency{
+				Code:       "EUR",
+				Name:       "Euro",
+				Symbol:     "€",
+				MinorUnits: 2,
+			},
+			hasError: false,
+		},
+		{
+			symbol:   "£",
+			expected: nil,
+			hasError: true,
+		},
+		{
+			symbol:   "XYZ",
+			expected: nil,
+			hasError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.symbol, func(t *testing.T) {
+			result, err := currency.FindUniqueSymbolCurrency(test.symbol)
+			if test.hasError {
+				assert.Error(t, err)
+				assert.Nil(t, result)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expected, result)
+			}
 		})
 	}
 }
